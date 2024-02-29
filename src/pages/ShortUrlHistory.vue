d6191d9998559615165d6c03e2d2f2c9da80764f
Roy <roy@gastom.com.tw>
2024-02-29 18:34:00 +0800

    adjust RWD

--- src/pages/ShortUrlHistory.vue
+++ src/pages/ShortUrlHistory.vue
@@ -4,13 +4,13 @@
       unelevated to="/" label="<Back" no-caps
     />
     <div class="tw-flex tw-justify-center">
-      <div class="tw-w-3/4 tw-border tw-shadow-inner tw-rounded tw-p-4">
+      <div class="tw-w-full lg:tw-w-3/4 tw-border tw-shadow-inner tw-rounded tw-p-4">
         <p class="tw-text-gray-600 tw-text-xl tw-text-center">Your History Record</p>
         <q-list bordered class="tw-rounded tw-shadow-inner tw-mt-4">
           <q-item clickable v-ripple class="tw-border-b-2 tw-border-gray-200">
-            <div class="tw-w-full tw-grid tw-grid-cols-5 tw-gap-4 tw-place-items-center">
-              <span>日期</span>
-              <span class="tw-col-span-2">短網址</span>
+            <div class="tw-w-full tw-grid tw-grid-cols-4 md:tw-grid-cols-5 tw-gap-4 tw-place-items-center">
+              <span class="tw-col-span-2 md:tw-col-span-1">日期</span>
+              <span class="tw-col-span-2 tw-hidden md:tw-block">短網址</span>
               <span>連結</span>
               <span>複製</span>
             </div>
@@ -18,9 +18,9 @@
           <q-item clickable v-ripple class="tw-border-b-2 tw-border-gray-200"
             v-for="item in historyArr" :key="item.ID.S"
           >
-            <div class="tw-w-full tw-grid tw-grid-cols-5 tw-gap-4 tw-place-items-center">
-              <span>{{item.Date.S}}</span>
-              <span class="tw-col-span-2">https://brief-url.link/url_api/{{item.ID.S}}</span>
+            <div class="tw-w-full tw-grid tw-grid-cols-4 md:tw-grid-cols-5 tw-gap-4 tw-place-items-center">
+              <span class="tw-col-span-2 md:tw-col-span-1">{{item.Date.S}}</span>
+              <span class="tw-col-span-2 tw-hidden md:tw-block">https://brief-url.link/url_api/{{item.ID.S}}</span>
               <q-btn color="white" text-color="orange" class="tw-text-lg" target="_blank"
                 unelevated :href="`https://brief-url.link/url_api/${item.ID.S}`" 
                 icon="link"
